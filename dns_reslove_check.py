79bf6c8d8f78c7ac0534aaf8bda900985d1bcfad
scotte666 <147806277+scotte666@users.noreply.github.com>
2023-10-17 18:24:10 +0800

    Update dns_reslove_check.py

--- dns_reslove_check.py
+++ dns_reslove_check.py
@@ -1,8 +1,11 @@
+
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 import  dns.resolver
 import subprocess
-domain_list=["www.goole.com","www.baidu.com"]
+import requests
+import json
+domain_list=["www.baidu.com","google.com"]
 dns_list=["114.114.114.114","119.29.29.29","223.5.5.5","117.50.10.10","123.125.81.6"]
 def sh():
     for d in domain_list:
@@ -20,11 +23,12 @@ def check_domain_available():
                 ms.nameservers=[i]
                 answer=ms.query(d,"A")
                 host=["88.88.88.88"]
-                for i in answer:
-                    if str(i)  in host:
+                for q in answer:
+                    if str(q)  in host:
                         print("域名正常")
                     else:
-                        print("域名解析一场")
+                        print("域名:"+ d + "在dns" + i + "解析异常")
+                        requests.post(url="https://api.telegram.org/bot633ddd52:ALZNqtrUDm2c/sendMessage",data={"chat_id":"-98856454652433130","text":d + "在dns" + i +"域名解析异常"})
             except dns.resolver.NoNameservers:
                 pass
 check_domain_available()
